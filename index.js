66a51dda669d92a21c2698756cdc7355e8c56c64
Orton Bright <ortonb110@gmail.com>
2023-05-24 23:54:35 +0000

    Minimal changes

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const btn = document.getElementById("btn-toggle");
 const navigation = document.getElementById("navigation");
+
 const toggleNavBar = () => {
   navigation.classList.toggle("active");
   document.body.classList.toggle("disable");
+  document.getElementById('input').classList.toggle("active");
+  document.getElementById('btn-input').classList.toggle("active");
   btn.classList.toggle('active');
 };
 
